00dc9ffac1832cd6a2741214cf6e682da60277bc
EDUCON\ddolp <ddolpal88@gmail.com>
2023-12-12 16:31:45 +0900

    3강 박스 잘 그려야 앱 잘 만듭니다 .

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,31 @@ void main() {
 }
 class  MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar( title : Text('앱임'),  ),
-        body: Text('안녕'),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 70,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-              Icon(Icons.phone),
-              Icon(Icons.message),
-              Icon(Icons.contact_page),
-            ],
-            ),
+        home: Scaffold(
+          appBar: AppBar(title: Text('앱임'),),
+          body: Align(
+            alignment: Alignment.,
+            child: Container(
+              width: double.infinity, height: 55, color:Colors.blue,
+
+              // margin: EdgeInsets.all(20), // 외부 여백 shift box
+
+              // padding: EdgeInsets.all(20),  // 내부 글씨 내부 박스 벽부터 거리
+
+              //margin: EdgeInsets.fromLTRB(0, 30, 0, 0) // 마진을 좌, 우, 상, 하 다 줄수 있다.
+
+              // 박스 데코 디자인
+              //  decoration: BoxDecoration( // 테두리 주기
+              //  border: Border.all(color: Colors.black),
+              //),
+
+             ),
           ),
-        ),
-      ),
+        )
     );
   }
 }
