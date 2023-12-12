049a8f02cbb64f69cda4eddf4883253df1db4c7a
EDUCON\ddolp <ddolpal88@gmail.com>
2023-12-12 16:00:36 +0900

    2강 가로 세로 배치하는 법과 Scaffold        이슈 : 전구로 container 감싸기                   title로 글씨 넣기, SizedBox로 대체하기

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,23 @@ class  MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Container(width: 50, height: 50, color: Colors.blue)
-            //
-      )
+      home: Scaffold(
+        appBar: AppBar( title : Text('앱임'),  ),
+        body: Text('안녕'),
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page),
+            ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
