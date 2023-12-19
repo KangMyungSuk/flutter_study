500172744f55c5a812e0e11df4421e9d43c274e8
EDUCON\ddolp <ddolpal88@gmail.com>
2023-12-19 13:25:02 +0900

    5강 1 flexible 강의 1/3

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,14 @@ class  MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          //appBar: AppBar(title: Text('앱임'),),
-          //appBar: AppBar(leading: Icon(Icons.star), title: Text('SolidSchema'), ),
-          appBar: AppBar(actions: [Icon(Icons.star), Icon(Icons.star)], title: Text('SolidSchema'), ),
-           body: SizedBox(),
-        ),
+            appBar: AppBar(),
+            body: Row(
+              children: [
+                Expanded(child: Container(color:Colors.blue)),
+                Container(width : 100, color:Colors.green),
+              ],
+            )
+        )
     );
   }
 }
-
-
-
-          /* 정렬  컨테이너 박스 좌우로 넓게 3장
-          body: Align(
-            alignment: Alignment.center,
-            child: Container(
-              width: double.infinity, height: 55, color:Colors.blue,
-          */
-              // margin: EdgeInsets.all(20), // 외부 여백 shift box
-
-              // padding: EdgeInsets.all(20),  // 내부 글씨 내부 박스 벽부터 거리
-
-              //margin: EdgeInsets.fromLTRB(0, 30, 0, 0) // 마진을 좌, 우, 상, 하 다 줄수 있다.
-
-              // 박스 데코 디자인
-              //  decoration: BoxDecoration( // 테두리 주기
-              //  border: Border.all(color: Colors.black),
-              //),
-
