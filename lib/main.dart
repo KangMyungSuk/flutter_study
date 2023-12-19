b3f2a0cf6e7678132bb23ac168cd40de4ec06816
EDUCON\ddolp <ddolpal88@gmail.com>
2023-12-19 16:01:51 +0900

    6강 2. 커스텀 위젯에서 변수로 저장하여 사용하는 방법

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
+var a = SizedBox(
+  child: Text('안녕'),
+);
+
 class  MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -11,21 +16,9 @@ class  MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(),
-          body:ShopItem(),
-
+          body: a,
         )
     );
   }
 }
 
-
-class ShopItem extends StatelessWidget {
-  const ShopItem({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      child: Text('안녕'),
-    );
-  }
-}
